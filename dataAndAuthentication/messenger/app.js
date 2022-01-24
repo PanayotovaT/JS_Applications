db7708ad0073e759a9d41ef483e4f2314ff31bb9
fortalesi <75318821+fortalesi@users.noreply.github.com>
2022-01-24 18:03:42 +0200

    Added a new exercise

---
+++ dataAndAuthentication/messenger/app.js
@@ -0,0 +1,46 @@
+await function attachEvents() {
+
+    document.getElementById('submit').addEventListener('click', async () => {
+        const author = document.getElementById('author').value;
+        const content = document.getElementById('content').value;
+        await sendMessage({author, content});
+
+        document.getElementById('author').value = '';
+        document.getElementById('content').value = '';
+
+        getMessages();
+
+    });
+
+
+
+    document.getElementById('refresh').addEventListener('click', getMessages)
+   getMessages();
+
+   getMessages();
+//    setInterval(getMessages, 5000); --- autoRefresh of the page
+}
+
+attachEvents();
+
+async function getMessages() {
+
+    const response  = await fetch('http://localhost:3030/jsonstore/messenger');
+    const data = await response.json();
+
+    const messages  = Object.values(data).map(value => `${value.author}: ${value.content}`).join('\n');
+ 
+    
+    document.getElementById('messages').value = messages;
+}
+
+async function sendMessage(message) {
+    const response = await fetch('http://localhost:3030/jsonstore/messenger', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify(message)
+    });
+
+    const data = await response.json();
+   
+}
